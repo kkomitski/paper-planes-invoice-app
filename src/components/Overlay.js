b01e2205860dc53fe72ceea0f82844cebc09bcda
Kiril Komitski <k.komitski94@gmail.com>
2022-02-05 15:47:34 +0000

    finished input overlay

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,12 +1,15 @@
 import React, { useRef, useState } from 'react';
 import plus from '../assets/plus-circle-solid.svg';
 import '../App.css';
+import ListItem from './ListItem';
 
 export default function Overlay() {
 	const [overlayContainerState, setOverlayContainerState] = useState('closed');
+	const [items, setItems] = useState([<ListItem />]);
 
 	const plusBtn = useRef();
 	const cancelBtn = useRef();
+	// const itemList = useRef();
 
 	const handleClick = () => {
 		console.log('click');
@@ -15,6 +18,10 @@ export default function Overlay() {
 		} else {
 			setOverlayContainerState('closed');
 		}
+	};
+
+	const addNewItem = () => {
+		setItems((items) => [...items, <ListItem />]);
 	};
 
 	return (
@@ -29,7 +36,7 @@ export default function Overlay() {
 				{/* <h1 className='overlay-title'>New Invoice</h1> */}
 				<fieldset style={{ outline: 'none', border: 'none' }} className='new-invoice'>
 					<form className='new-invoice-input'>
-						<h4 className='input-title'>Invoice From:</h4>
+						{/* <h4 className='input-title'>Invoice From:</h4>
 						<div className='invoice-from'>
 							<div className='company'>
 								<h2>Company</h2>
@@ -51,7 +58,7 @@ export default function Overlay() {
 								<h2>Country</h2>
 								<input className='new-invoice-input' type='text' />
 							</div>
-						</div>
+						</div> */}
 						<h4 className='input-title'>Invoice To:</h4>
 						<div className='invoice-to'>
 							<div className='client'>
@@ -62,9 +69,47 @@ export default function Overlay() {
 								<h2>Email</h2>
 								<input type='email' className='new-invoice-input' />
 							</div>
-							<div className='Street'>
+							<div className='client-street'>
 								<h2>Street</h2>
 								<input type='text' className='new-invoice-input' />
+							</div>
+							<div className='client-city'>
+								<h2>City</h2>
+								<input type='text' className='new-invoice-input' />
+							</div>
+							<div className='client-postcode'>
+								<h2>Postcode</h2>
+								<input type='text' className='new-invoice-input' />
+							</div>
+							<div className='client-country'>
+								<h2>Country</h2>
+								<input type='text' className='new-invoice-input' />
+							</div>
+							<br />
+							<div className='invoice-date'>
+								<h2>Invoice Date</h2>
+								<input type='text' className='new-invoice-input' />
+							</div>
+							<div className='payment-expected'>
+								<h2>Payment Expected</h2>
+								<input type='text' className='new-invoice-input' />
+							</div>
+							<div className='job-description'>
+								<h2>Job Description</h2>
+								<input type='text' className='new-invoice-input' />
+							</div>
+							<h4 className='input-title items'>Items:</h4>
+							<div onClick={() => addNewItem()} className='add-new-item'>
+								+ Add New Item
+							</div>
+							<div className='items-list'>
+								{items.map((item, i) => {
+									return (
+										<div className='item' key={i}>
+											{item}
+										</div>
+									);
+								})}
 							</div>
 						</div>
 					</form>
