96b4004d3b5778d4a35e264691ae71f39cbd3bbf
Kiril Komitski <k.komitski94@gmail.com>
2022-02-16 17:35:11 +0000

    Update profile fully working

--- src/components/Authentication/UpdateProfile.js
+++ src/components/Authentication/UpdateProfile.js
@@ -5,7 +5,13 @@ import { collection, doc, getDoc, onSnapshot, setDoc } from 'firebase/firestore'
 import { db } from '../../firebase-config';
 import { confirmPasswordReset } from 'firebase/auth';
 import { useEffect } from 'react';
-import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytes,
+	uploadBytesResumable,
+} from 'firebase/storage';
 
 export function UpdateProfile({ activeComponent, setActiveComponent }) {
 	const emailRef = useRef();
@@ -29,24 +35,26 @@ export function UpdateProfile({ activeComponent, setActiveComponent }) {
 	const [loading, setLoading] = useState(false);
 	const [formData, setFormdata] = useState(blankForm);
 	const [userInfo, setUserInfo] = useState([]);
-	const [image, setImage] = useState(null);
+	const [logoState, setLogoState] = useState('without-logo');
 	const [logo, setLogo] = useState(<h2 className='company-logo'>logo</h2>);
 
 	const navigate = useNavigate();
 
 	const storage = getStorage();
-	const logoRef = ref(storage, `logos/${currentUser.uid}`);
-	const currentLogo = ref(storage, `logos/${currentUser.uid}`);
 
 	const getCurrentUserInfo = () => {
 		const userInfoRef = doc(db, 'users', currentUser.email);
 		onSnapshot(userInfoRef, (snapshot) => {
 			const currentUserInfo = snapshot.data();
 			setUserInfo(currentUserInfo);
+			if (userInfo.logo) {
+				setLogoState('with-logo');
+				setLogo(<img src={userInfo.logo} alt='logo' className='logo-img' />);
+			} else {
+				setLogoState('without-logo');
+				setLogo(<h2 className='company-logo'>logo</h2>);
+			}
 		});
-		// 	if (userInfo.logo) {
-		// 		setLogo(<img src={`gs://my-test-app-c99a2.appspot.com/${currentUser.uid}`} alt='logo' />);
-		// 	}
 	};
 
 	const handleLogout = async () => {
@@ -61,12 +69,11 @@ export function UpdateProfile({ activeComponent, setActiveComponent }) {
 		}
 	};
 
-	const infoChange = async (data) => {
+	const postFormData = async (data) => {
 		await setDoc(doc(db, 'users', currentUser.email), { ...data }, { merge: true });
 	};
 
 	const emailPassChange = async () => {
-		// e.preventDefault();
 		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
 			return setError('Passwords do not match');
 		}
@@ -107,194 +114,196 @@ export function UpdateProfile({ activeComponent, setActiveComponent }) {
 			formData &&
 			(!emailRef.current.value || !passwordRef.current.value || !passwordConfirmRef.current.value)
 		) {
-			infoChange(formData);
-			console.log('change info only');
+			// Change form data only
+			postFormData(formData);
 		} else if (
 			!formData &&
 			(emailRef.current.value || passwordRef.current.value || passwordConfirmRef.current.value)
 		) {
+			// Change email/pass only
 			emailPassChange();
-			console.log('change email and pass');
 		} else {
-			infoChange(formData);
+			// Change both
+			postFormData(formData);
 			emailPassChange();
-			console.log('change both');
 		}
 	};
 
-	const uploadFile = async () => {
-		uploadBytes(logoRef, image)
-			.then((snapshot) => {
-				console.log(snapshot);
-				// getCurrentUserInfo();
-			})
-			.then(setDoc(doc(db, 'users', currentUser.email), { logo: true }, { merge: true }));
-	};
-
-	const uploadCompanyLogo = (e) => {
+	let progress = 0;
+	const uploadCompanyLogo = async (e) => {
 		if (e.target.files[0]) {
-			setImage(e.target.files[0]);
-			uploadFile();
+			const format = e.target.files[0].name.split('.').at(-1);
+			const currentLogoRef = ref(storage, `logos/${currentUser.uid}.${format}`);
+			const uploadTask = uploadBytesResumable(currentLogoRef, e.target.files[0]);
+
+			uploadTask.on(
+				'state_changed',
+				(snapshot) => {
+					progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					setError(`${progress}% uploaded...`);
+					progress !== 100 ? setError(`Logo ${progress.toFixed(0)}% uploaded..`) : setError('');
+				},
+				(error) => {
+					console.log(error);
+				},
+				() => {
+					getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+						await setDoc(
+							doc(db, 'users', currentUser.email),
+							{ logo: downloadURL },
+							{ merge: true }
+						);
+					});
+				}
+			);
 		}
 	};
 
-	const printForm = () => {
-		// uploadFile();
-	};
-
-	useEffect(() => getCurrentUserInfo(), []);
+	useEffect(() => getCurrentUserInfo(), [progress, userInfo.logo]);
 
 	return (
-		<>
-			<section className='main-section'>
-				<article className='main-section__container profile-update'>
-					<div className='title-container profile-title'>
-						<h1 className='page-title'>Profile</h1>
-					</div>
-					<div className='profile-page-container'>
-						<fieldset className='business-info-fieldset'>
-							<div onClick={() => printForm()} className='name-and-logo'>
-								{/* <h1 className='company-name'>Company Name</h1> */}
-								<input
-									type='text'
-									defaultValue={userInfo['company-name']}
-									name='company-name'
-									onChange={(e) => addInfoToForm(e)}
-									className='company-name'
-								/>
-								<div
-									ref={photoInputForm}
-									onClick={() => updateCompanyLogo.current.click()}
-									// onSubmit={(e) => e.preventDefault()}
-									className='company-logo-container'
-									id='photo-input-form'
-								>
-									<input
-										type='file'
-										ref={updateCompanyLogo}
-										id='photo-input'
-										onChange={(e) => uploadCompanyLogo(e)}
-									/>
-									{logo}
-									{/* <h2 className='company-logo'>{logo}</h2> */}
-								</div>
-							</div>
-							<div className='error update-profile-error'>{error}</div>
-							<div className='update-street'>
-								<h2 className='item-attribute'>Street</h2>
-								<input
-									ref={updateStreetRef}
-									defaultValue={userInfo['street']}
-									name='street'
-									onChange={(e) => addInfoToForm(e)}
-									className='update-info-input'
-									type='address'
-								/>
-							</div>
-							<div className='update-city'>
-								<h2 className='item-attribute'>City</h2>
-								<input
-									ref={updateCityRef}
-									defaultValue={userInfo['city']}
-									name='city'
-									onChange={(e) => addInfoToForm(e)}
-									className='update-info-input'
-									type='text'
-								/>
-							</div>
-							<div className='update-postcode'>
-								<h2 className='item-attribute'>Postcode</h2>
-								<input
-									ref={updatePostcodeRef}
-									defaultValue={userInfo['postcode']}
-									name='postcode'
-									onChange={(e) => addInfoToForm(e)}
-									className='update-info-input'
-									type='text'
-								/>
-							</div>
-							<div className='update-country'>
-								<h2 className='item-attribute'>Country</h2>
-								<input
-									ref={updateCountryRef}
-									defaultValue={userInfo['country']}
-									name='country'
-									onChange={(e) => addInfoToForm(e)}
-									className='update-info-input'
-									type='text'
-								/>
-							</div>
-							<div className='update-business-number'>
-								<h2 className='item-attribute'>Business Number</h2>
-								<input
-									ref={updateBusinessNumberRef}
-									defaultValue={userInfo['business-number']}
-									name='business-number'
-									onChange={(e) => addInfoToForm(e)}
-									className='update-info-input'
-									type='text'
-								/>
-							</div>
-							<div className='update-vat-registration'>
-								<h2 className='item-attribute'>VAT registration</h2>
-								<input
-									ref={updateVATregRef}
-									defaultValue={userInfo['vat-registration']}
-									name='vat-registration'
-									onChange={(e) => addInfoToForm(e)}
-									className='update-info-input'
-									type='text'
-								/>
-							</div>
-							<br className='profile-title' />
-							<div className='update-email'>
-								<h2 className='item-attribute'>Email</h2>
-								<input
-									type='email'
-									id='email'
-									className='update-info-input'
-									ref={emailRef}
-									required
-									defaultValue={currentUser.email}
-								/>{' '}
-							</div>
-							<div className='update-password'>
-								<h2 className='item-attribute'>New Password</h2>
-								<input
-									type='password'
-									id='password'
-									className='update-info-input'
-									ref={passwordRef}
-									placeholder='Leave blank to keep the same'
-									defaultValue={null}
-								/>
-							</div>
-							<div className='update-password-confirm'>
-								<h2 className='item-attribute'>Confirm New Password</h2>
-								<input
-									type='password'
-									id='password-confirm'
-									className='update-info-input'
-									ref={passwordConfirmRef}
-									placeholder='Leave blank to keep the same'
-								/>
-							</div>
-							<div onClick={handleSubmit} className='add-items update-info' disabled={loading}>
-								Update
-							</div>
+		<section className='main-section'>
+			<article className='main-section__container profile-update'>
+				<div className='title-container profile-title'>
+					<h1 className='page-title'>Profile</h1>
+				</div>
+				<div className='profile-page-container'>
+					<fieldset className='business-info-fieldset'>
+						<div className='name-and-logo'>
+							<input
+								type='text'
+								defaultValue={userInfo['company-name']}
+								name='company-name'
+								onChange={(e) => addInfoToForm(e)}
+								className='company-name'
+							/>
 							<div
-								onClick={() => setActiveComponent('invoices')}
-								className='add-items cancel-button'
+								ref={photoInputForm}
+								onClick={() => updateCompanyLogo.current.click()}
+								className={`company-logo-container ${logoState}`}
+								id='photo-input-form'
 							>
-								Cancel
+								<input
+									type='file'
+									ref={updateCompanyLogo}
+									id='photo-input'
+									onChange={(e) => uploadCompanyLogo(e)}
+								/>
+								{logo}
 							</div>
-							<div type='link' className='add-items logout-button' onClick={handleLogout}>
-								Log Out
-							</div>
-						</fieldset>
-					</div>
-				</article>
-			</section>
-		</>
+						</div>
+						<div className='error update-profile-error'>{error}</div>
+						<div className='update-street'>
+							<h2 className='item-attribute'>Street</h2>
+							<input
+								ref={updateStreetRef}
+								defaultValue={userInfo['street']}
+								name='street'
+								onChange={(e) => addInfoToForm(e)}
+								className='update-info-input'
+								type='address'
+							/>
+						</div>
+						<div className='update-city'>
+							<h2 className='item-attribute'>City</h2>
+							<input
+								ref={updateCityRef}
+								defaultValue={userInfo['city']}
+								name='city'
+								onChange={(e) => addInfoToForm(e)}
+								className='update-info-input'
+								type='text'
+							/>
+						</div>
+						<div className='update-postcode'>
+							<h2 className='item-attribute'>Postcode</h2>
+							<input
+								ref={updatePostcodeRef}
+								defaultValue={userInfo['postcode']}
+								name='postcode'
+								onChange={(e) => addInfoToForm(e)}
+								className='update-info-input'
+								type='text'
+							/>
+						</div>
+						<div className='update-country'>
+							<h2 className='item-attribute'>Country</h2>
+							<input
+								ref={updateCountryRef}
+								defaultValue={userInfo['country']}
+								name='country'
+								onChange={(e) => addInfoToForm(e)}
+								className='update-info-input'
+								type='text'
+							/>
+						</div>
+						<div className='update-business-number'>
+							<h2 className='item-attribute'>Business Number</h2>
+							<input
+								ref={updateBusinessNumberRef}
+								defaultValue={userInfo['business-number']}
+								name='business-number'
+								onChange={(e) => addInfoToForm(e)}
+								className='update-info-input'
+								type='text'
+							/>
+						</div>
+						<div className='update-vat-registration'>
+							<h2 className='item-attribute'>VAT registration</h2>
+							<input
+								ref={updateVATregRef}
+								defaultValue={userInfo['vat-registration']}
+								name='vat-registration'
+								onChange={(e) => addInfoToForm(e)}
+								className='update-info-input'
+								type='text'
+							/>
+						</div>
+						<br className='profile-title' />
+						<div className='update-email'>
+							<h2 className='item-attribute'>Email</h2>
+							<input
+								type='email'
+								id='email'
+								className='update-info-input'
+								ref={emailRef}
+								required
+								defaultValue={currentUser.email}
+							/>{' '}
+						</div>
+						<div className='update-password'>
+							<h2 className='item-attribute'>New Password</h2>
+							<input
+								type='password'
+								id='password'
+								className='update-info-input'
+								ref={passwordRef}
+								placeholder='Leave blank to keep the same'
+								defaultValue={null}
+							/>
+						</div>
+						<div className='update-password-confirm'>
+							<h2 className='item-attribute'>Confirm New Password</h2>
+							<input
+								type='password'
+								id='password-confirm'
+								className='update-info-input'
+								ref={passwordConfirmRef}
+								placeholder='Leave blank to keep the same'
+							/>
+						</div>
+						<div onClick={handleSubmit} className='add-items update-info' disabled={loading}>
+							Update
+						</div>
+						<div onClick={() => setActiveComponent('invoices')} className='add-items cancel-button'>
+							Cancel
+						</div>
+						<div type='link' className='add-items logout-button' onClick={handleLogout}>
+							Log Out
+						</div>
+					</fieldset>
+				</div>
+			</article>
+		</section>
 	);
 }
--- src/components/SingleInvoice.js
+++ src/components/SingleInvoice.js
@@ -1,9 +1,14 @@
+import { collection, doc, setDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import '../App.css';
+import { useAuth } from '../context/AuthContext';
+import { db } from '../firebase-config';
 
 export default function SingleInvoice({ id, client, total, createdAt, status, due }) {
 	const [currentStatus, setStatus] = useState('');
 	const [date, setDueDate] = useState('');
+
+	const { currentUser } = useAuth();
 
 	const getDate = async () => {
 		const creationDate = new Date(createdAt.toDate());
@@ -16,9 +21,7 @@ export default function SingleInvoice({ id, client, total, createdAt, status, du
 		setDueDate(displayDue);
 	};
 
-	// Draft
 	const getStatus = () => {
-		// await status;
 		switch (status) {
 			case 'Paid':
 				setStatus('paid');
@@ -37,27 +40,46 @@ export default function SingleInvoice({ id, client, total, createdAt, status, du
 		}
 	};
 
+	const statusToggle = () => {
+		if (status === 'Pending' || status === 'Overdue') {
+			setDoc(
+				doc(db, 'users', currentUser.email, 'Invoices', id),
+				{ status: 'Paid' },
+				{ merge: true }
+			);
+		} else if (status === 'Paid') {
+			setDoc(
+				doc(db, 'users', currentUser.email, 'Invoices', id),
+				{ status: 'Pending' },
+				{ merge: true }
+			);
+		}
+	};
+
 	useEffect(() => {
 		getStatus();
 		getDate();
 	}, [status]);
 
 	return (
-		<div className='invoice-container'>
-			<div className='name-and-id'>
-				<h3 className='id'>#{id.slice(0, 5)}</h3>
-				<h2 className='name'>{client}</h2>
-			</div>
-			<div className='due-and-status'>
-				<div className='due-and-total'>
-					<h2 className='due'>Due {date}</h2>
-					<h1 className='total'>£ {parseFloat(total).toFixed(2)}</h1>
+		<>
+			<div className='invoice-container'>
+				<div className='name-and-id'>
+					<h3 className='id'>#{id.slice(0, 5)}</h3>
+					<h2 className='name'>{client}</h2>
 				</div>
-				<div className={`status-container ${currentStatus}`}>
-					<div className='dot'>•</div>
-					<h4 className='status'>{status}</h4>
+				<div className='due-and-status'>
+					<div className='due-and-total'>
+						<h2 className='due'>Due {date}</h2>
+						<h1 className='total'>£ {parseFloat(total).toFixed(2)}</h1>
+					</div>
+					<div onClick={() => statusToggle()} className={`status-container ${currentStatus}`}>
+						<div className='dot'>•</div>
+						<h4 className='status'>{status}</h4>
+					</div>
 				</div>
 			</div>
-		</div>
+			<div className={`full-info`}></div>
+		</>
 	);
 }
